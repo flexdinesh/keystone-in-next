912efcf60d5aea7f7354e7dd8bb46acb169a5833
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-10-28 15:58:35 +1100

    Safer types

--- lib/keystone-context.ts
+++ lib/keystone-context.ts
@@ -1,16 +1,10 @@
+import { Context } from ".keystone/types";
 import { getContext } from "@keystone-6/core/context";
 import config from "../keystone";
 import * as PrismaModule from ".prisma/client";
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var keystoneContext: ReturnType<typeof getContext>;
-}
+export const keystoneContext: Context =
+  (globalThis as any).keystoneContext || getContext(config, PrismaModule);
 
-export const keystoneContext =
-  global.keystoneContext || getContext(config, PrismaModule);
-
-if (process.env.NODE_ENV !== "production") {
-  global.keystoneContext = keystoneContext;
-}
+if (process.env.NODE_ENV !== "production")
+  (globalThis as any).keystoneContext = keystoneContext;

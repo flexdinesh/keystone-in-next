03f5a512461958bfbef623fc57072ba8f11b5d16
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-11-03 15:48:03 +1100

    Add GraphQL handlers

---
+++ pages/api/graphql-apollo.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer } from "apollo-server-micro";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "@apollo/server-plugin-landing-page-graphql-playground";
+import { keystoneContext } from "../../lib/keystone-context";
+
+const apolloServer = new ApolloServer({
+  schema: keystoneContext.graphql.schema,
+  context: keystoneContext,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql-apollo",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
---
+++ pages/api/graphql-yoga.ts
@@ -0,0 +1,19 @@
+import { createServer } from "@graphql-yoga/node";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { keystoneContext } from "../../lib/keystone-context";
+
+export const config = {
+  api: {
+    // Disable body parsing (required for file uploads)
+    bodyParser: false,
+  },
+};
+
+export default createServer<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  schema: keystoneContext.graphql.schema,
+  context: keystoneContext
+  // endpoint: "/api/graphql-yoga",
+});

eb080faa2d34b937d7b9eb307c3ee8e59ba8b230
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-07 13:35:23 +1100

    Try marking .prisma as an external in next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.externals = [...(config.externals || []), ".prisma/client"];
+    // Important: return the modified config
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
